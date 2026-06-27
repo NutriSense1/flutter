921366913a5d0a9f8911011c8ed542223f6da990
Neil Surjiani <surjianineil@gmail.com>
2026-06-27 21:56:59 +0530

    bug

--- lib/screens/coach/coach_screen.dart
+++ lib/screens/coach/coach_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../core/theme/app_colors.dart';
 import '../../core/theme/app_typography.dart';
+import '../../providers/auth_provider.dart';
 import '../../providers/user_provider.dart';
 import '../../providers/food_log_provider.dart';
 import '../../providers/tracking_providers.dart';
